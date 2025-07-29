36344875e2f071f327ad0cd11a45cca50b94b9f7
Ikhsan Khoerul <khoerulih.r@gmail.com>
2025-07-29 12:48:03 +0700

    add function for generate address and phone

---
+++ data/address.go
@@ -0,0 +1,6 @@
+package data
+
+var address = map[string][]string{
+	SUBTYPE_ADDRESS_STREET: {"Tjut Meutia", "Mohammad Hoesni Thamrin", "Dr. K.H. Idham Chalid", "Frans Kaisiepo", "Dr. G.S.S.J. Ratulangi", "Ir. H. Djuanda Kartawidjaja", "Dr. (H.C.) Ir. Soekarno", "Dr. (H.C.) Drs. Mohammad Hatta"},
+	SUBTYPE_ADDRESS_CITY:   {"Jakarta", "Surabaya", "Bandung", "Medan", "Semarang", "Makassar", "Palembang", "Depok", "Tangerang", "Bekasi"},
+}
---
+++ data/name.go
@@ -0,0 +1,14 @@
+package data
+
+var name = [...]string{
+	"Ani",
+	"Budi",
+	"Citra",
+	"Eko",
+	"Dewi",
+	"Fitri",
+	"Gita",
+	"Hana",
+	"Irfan",
+	"Jasmine",
+}
